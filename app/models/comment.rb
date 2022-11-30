df6c4e0c49f5636829c6a7dc23f4bc436f1583f3
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-29 21:38:44 -0500

    Update relations' parameters in 'Like' class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
-  after_create :increment_counter
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  after_save :update_comment_counter
+
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
