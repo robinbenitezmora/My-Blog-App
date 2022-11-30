c23f90a99ba00168e726cdfd89b11095d0f290ee
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-29 21:30:08 -0500

    Update relations' parameters in 'Comment' class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_create :increment_counter
 end
