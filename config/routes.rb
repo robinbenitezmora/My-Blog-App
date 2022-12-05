1fee264cc4d3ae3904a85955b3c604ed1b056660
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-05 14:45:49 -0500

    Set code lines in order to give funtionality to 'Posts' controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  end
 end
