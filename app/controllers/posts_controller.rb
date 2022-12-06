3c6770011dda7444f31d7b0fa4bb429bc5231711
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-06 14:59:40 -0500

    Modify controllers for 'posts'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @user = params[:user_id]
-    @posts = Post.where(user_id: @user)
+    @user = User.find_by!(id: params[:user_id])
   end
 
   def show
-    @user = params[:user_id]
-    @post = Post.find_by!(id: params[:id], user_id: params[:user_id])
+    @user = User.find_by!(id: params[:user_id])
+    @post = Post.find_by!(id: params[:id])
   end
 end
