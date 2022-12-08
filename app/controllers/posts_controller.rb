7a1bb73dd2113c693d7b5f46d029e0681ea2a889
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-07 20:00:51 -0500

    Modify code lines in 'posts_controller.rb' file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,28 @@ class PostsController < ApplicationController
   def show
     @user = User.find_by!(id: params[:user_id])
     @post = Post.find_by!(id: params[:id])
+    @post = Post.includes(:comments, :author).find_by!(author_id: params[:user_id], id: params[:id])
+    @user = current_user
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    parameters = post_params
+    post = Post.new(author_id: current_user.id, title: parameters[:title], text: parameters[:text])
+    post.save
+
+    if post.save
+      redirect_to user_post_path(current_user)
+    else
+      redirect_to new_user_post_path(current_user)
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
