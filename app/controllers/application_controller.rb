d4d4ed7194649d8fd905d7cc8db73924b90e5627
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-13 17:58:19 -0500

    Add methods in 'application_controller.rb' file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
+  def after_inactive_sign_up_path_for(_resource)
+    flash[:success] = 'It is a pleasure you are here again!!!'
+    new_user_session_path
+  end
+
+  def update_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :email, :password, :password_confirmation, :bio)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :email, :password, :current_password, :bio)
+    end
+  end
 end
