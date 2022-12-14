11f522eb3d8f2fdd0ff8e74e4c102ada7c8bf52a
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-14 12:31:44 -0500

    Definy scope or ability for the users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Post
+
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :manage, Like, author_id: user.id
+
+    return unless user.admin?
+
+    can :manage, :all
+
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
