1778d4129e7da17ef631665e7afa39718586f5bf
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-14 13:35:08 -0500

    Modify 'user.rb' file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+  has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   validates :name, presence: true
 
@@ -17,5 +17,11 @@ class User < ApplicationRecord
 
   def all_posts
     posts.order(created_at: :desc)
+  end
+
+  Roles = [ :admin, :default ]
+
+  def is?(requested_role)
+    self.role = requested_role.to_s
   end
 end
