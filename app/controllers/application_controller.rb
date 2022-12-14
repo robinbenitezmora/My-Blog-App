ae0630cbadfad2610dac60f1f708c2f1dda27273
robinbenitezmora <robinbenitez1990@gmail.com>
2022-12-13 21:23:27 -0500

    Correct linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  before_action :update_parameters, if: :devise_controller?
-    
-  end
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
+
+  def after_inactive_sign_up_path_for(_resource)
+    flash[:success] = 'It is nice to see you !'
+    new_user_session_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
@@ -13,12 +16,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 
-  def after_inactive_sign_up_path_for(_resource)
-    flash[:success] = 'It is a pleasure you are here again!!!'
-    new_user_session_path
-  end
-
-  def update_parameters
+  def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :photo, :email, :password, :password_confirmation, :bio)
     end
